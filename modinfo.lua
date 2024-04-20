64f5b6caea93477545d5ea4e1959680cce39d111
jcosmick <jcosmick@gmail.com>
2024-04-20 12:19:23 +0200

    edited modinfo

--- modinfo.lua
+++ modinfo.lua
@@ -3,9 +3,8 @@ description = ""
 author = "Sacco"
 version = "1.2"
 forumthread = ""
-client_only_mod = false
-dont_starve_compatible = false
-reign_of_giants_compatible = true
 dst_compatible = true
+client_only_mod = false
+all_clients_require_mod = true
 
 api_version = 10
